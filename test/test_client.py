663ffed8609dad6fdd452c6e2b7f6d7f5e2bdd30
AutomatedTester <david.burns@theautomatedtester.co.uk>
2016-04-25 11:14:09 +0100

    Reenabling timeout tests as they have been fixed in B6 of the server

--- test/test_client.py
+++ test/test_client.py
@@ -212,7 +212,6 @@ class TestClient(object):
         with pytest.raises(KeyError):
             self.client.timeouts({})
 
-    @pytest.mark.xfail(reason="see https://github.com/lightbody/browsermob-proxy/issues/414")
     def test_timeouts_all_key_values(self):
         """
         /proxy/:port/timeout
